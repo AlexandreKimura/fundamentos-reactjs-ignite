9c82da590a0cf0da35a8def829a90ebdf48c21fd
Alexandre Kimura <alexandremkimura@hotmail.com>
2021-07-09 11:11:47 -0300

    state concept

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,29 @@
+import { RepositoryItem } from "./RepositoryItem";
+
+const repository = {
+  name: 'unform',
+  description: 'Forms in React',
+  link: 'https://github.com/'
+}
+
+export function RepositoryList() {
+  return (
+    <section className="repostitory-list">
+      <h1>Lista de repositórios</h1>
+      <ul>
+        <RepositoryItem 
+          repository={repository}
+        />
+        <RepositoryItem 
+          repository={repository}
+        />
+        <RepositoryItem 
+          repository={repository}
+        />
+        <RepositoryItem 
+          repository={repository}
+        />
+      </ul>
+    </section>
+  )
+}
